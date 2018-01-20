6a624d0ca424817504cdc3d87700f52e8e4d0175
Dave Llewellyn <439837@gmail.com>
2018-01-20 17:43:25 +0000

    symbols now drawn on click

--- main.py
+++ main.py
@@ -18,33 +18,38 @@ SCREEN_SIZE = 490
 MARGIN = 10
 TILE_SIZE = 150
 HIGHLIGHT_SIZE = TILE_SIZE + 10
+NAUGHT = "o"
+CROSS = "x"
 
 
 def main():
+    pygame.init()
     global FPSCLOCK, DISPLAYSURF
 
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE), 0, 32)
     pygame.display.set_caption(TITLE)
 
-    pygame.init()
-
     # create a new game board
     board = Board()
+    user = Player(CROSS, "user")
+    computer = Player(NAUGHT, "computer")
+    current_player = user
 
     while True:
         for event in pygame.event.get():
             checkForExit(event)
 
             if event.type == MOUSEMOTION:
-                mouseX, mouseY = event.pos
-                board.highlight_under_mouse(mouseX, mouseY)
+                mouse_x, mouse_y = event.pos
+                board.highlight_under_mouse(mouse_x, mouse_y)
 
             if event.type == MOUSEBUTTONDOWN:
-                clickedIndex = board.get_clicked(mouseX, mouseY)
-                if clickedIndex > -1:
-                    print("tile ", clickedIndex, " was clicked")
-
+                if board.clicked(current_player, mouse_x, mouse_y):
+                    if current_player == user:
+                        current_player = computer
+                    else:
+                        current_player = user
 
         board.draw()
 
@@ -98,41 +103,49 @@ class Board:
     def get_tile(self, index):
         return self.tiles[index]
 
-    def highlight_under_mouse(self, mouseX, mouseY):
+    def highlight_under_mouse(self, mouse_x, mouse_y):
         for tile in self.tiles:
-            if tile.Rect.collidepoint(mouseX, mouseY):
+            if tile.rect.collidepoint(mouse_x, mouse_y):
                 tile.highlighted = True
             else:
                 tile.highlighted = False
 
-    def get_clicked(self, mouseX, mouseY):
+    def clicked(self, player, mouseX, mouseY):
         for i in range(0, len(self.tiles)):
             t = self.tiles[i]
-            if t.Rect.collidepoint(mouseX, mouseY):
-                return i
-        return -1
+            if t.rect.collidepoint(mouseX, mouseY):
+                return t.set_symbol(player)
+        return False
 
     def draw(self):
-        DISPLAYSURF.fill(BLACK)
+        DISPLAYSURF.fill(GREY)
 
         for tile in self.tiles:
-            if tile.highlighted:
-                pygame.draw.rect(DISPLAYSURF, WHITE, tile.highlightRect)
-
-            pygame.draw.rect(DISPLAYSURF, GREY, tile.Rect)
+            tile.draw()
 
 
 class Tile:
     value = None
     highlighted = False
-    Rect = None
+    rect = None
     highlightRect = None
+    symbol = None
 
     def __init__(self, xco, yco):
         self.value = None
-        self.Rect = pygame.Rect(xco, yco, TILE_SIZE, TILE_SIZE)
-        self.highlightRect = pygame.Rect(self.Rect.topleft[0] - 5, self.Rect.topleft[1] - 5, HIGHLIGHT_SIZE,
-                                         HIGHLIGHT_SIZE)
+        self.symbol = None
+        self.rect = pygame.Rect(xco, yco, TILE_SIZE, TILE_SIZE)
+
+        x = self.rect.topleft[0] - 5
+        y = self.rect.topleft[1] - 5
+        self.highlightRect = pygame.Rect(x, y, HIGHLIGHT_SIZE, HIGHLIGHT_SIZE)
+
+    def set_symbol(self, player):
+        if self.symbol is None:
+            self.symbol = Symbol(player.symbol, self)
+            return True
+
+        return False
 
     def is_empty(self):
         return self.value is None
@@ -142,6 +155,29 @@ class Tile:
 
     def set_value(self, value):
         self.value = value
+
+    def draw(self):
+        # draw the highlight around the tile if the mouse is over it.
+        if self.highlighted:
+            pygame.draw.rect(DISPLAYSURF, WHITE, self.highlightRect)
+
+        # draw the tile background
+        pygame.draw.rect(DISPLAYSURF, BLACK, self.rect)
+
+        if self.symbol is not None:
+            DISPLAYSURF.blit(self.symbol.text, self.symbol.text_rect)
+
+
+class Symbol:
+    text = None
+    text_rect = None
+
+    def __init__(self, value, tile):
+        self.basicFont = pygame.font.SysFont(None, 250)
+        self.text = self.basicFont.render(value, True, WHITE, None)
+        self.text_rect = self.text.get_rect()
+        self.text_rect.centerx = tile.rect.centerx
+        self.text_rect.centery = tile.rect.centery
 
 
 if __name__ == '__main__':
